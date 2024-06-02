9518e8a49c2874da4534daa4e1a8fd48a992a0df
anycodef <anycodefast1@gmail.com>
2024-06-01 19:01:58 -0500

    Update the routes and add the logo

--- Desarrollo/Fuentes/Frontend/terrai-frontend-app/src/App.js
+++ Desarrollo/Fuentes/Frontend/terrai-frontend-app/src/App.js
@@ -4,23 +4,46 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
+
     <Router>
       <div className='App'>
-        <nav>
-          <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
-          <li><Link to='/contact'>Contact</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route exact path='/' Component={Home} />
-          <Route exact path='/about' Component={About} />
-          <Route exact path='/contact' Component={Contact} />
-        </Routes>
+
+        <a href='/' className='logo-terrai'>
+          <img src='logo.svg' alt='Logo Terrai'/>
+        </a>
+
+        <header>
+          <nav className='navigation'>
+            <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+          </nav>
+          <nav>
+            <li><Link to='/login'>Log in</Link></li>
+            <li><Link to='/Signup'>Sign up</Link></li>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route exact path='/' Component={Home} />
+            <Route exact path='/about' Component={About} />
+            <Route exact path='/contact' Component={Contact} />
+            <Route exact path='/login' Component={Login} />
+            <Route exact path='/signup' Component={Signup} />
+          </Routes>
+        </main>
+
+        <footer>
+          <p>Footer</p>
+        </footer>
+
       </div>
     </Router>
   );
